2064161b16d16f49b059633556e347cafbd75fbf
bijayv <nrbijay52@gmail.com>
2024-07-27 14:28:42 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -16,7 +16,19 @@ def low_high_temp(temp):
   if temp <0:
     print('Temperature is LOW!')
   else:
-print('Temperature is HIGH!')
+    print('Temperature is HIGH!')
+
+def check_temp(temp):
+  if temp <=2.25 or temp >42.75:
+    warning_check_temp(temp)
+  else:
+    print('Temperature is NORMAL')
+
+def warning_check_temp(temp):
+  if temp <=2.25:
+    print('State of Charge is LOW WARNING')
+  else:
+    print('State of Charge is HIGH WARNING')
     
 def soc_check(soc):
   if soc <=20 or soc > 80:
@@ -45,8 +57,14 @@ def warning_check(soc):
 def charge_rate_check(ch):
   if ch > 0.8:
     print('Charge rate is out of range!')
-    return False
-  else: return True
+  else:
+    charge_rate_warning(ch)
+    
+def charge_rate_warning(ch):
+  if ch> 0.6:
+    print('Charge rate is WARNING')
+  else:
+    print('Charge rate is NORMAL')
     
 if __name__ == '__main__':
   battery_is_ok(25, 70, 0.7)
